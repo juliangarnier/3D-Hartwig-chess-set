46ad43724f6da15a49401f5776c919a155d54db1
dongrogers <dongrogers>
2019-05-30 18:49:42 -0700

    Added "moving into check" test

--- tests/check.test.js
+++ tests/check.test.js
@@ -8,6 +8,7 @@ test('in check', () => {
 });
 
 test('discovered check', () => {
+	//Piece moves and in doing so, creaes a check situation
   var chess = new libraries.Chess();
   chess.load("4k3/8/8/8/8/8/4K3/4Q3 w KQkq - 0 1");
   expect(chess.in_check()).toBe(false);
@@ -17,6 +18,7 @@ test('discovered check', () => {
 });
 
 test('double check', () => {
+	//Two pieces place a king in check, but not checkmate
   var chess = new libraries.Chess();
   chess.load("RNBK1B1R/PP3PPP/2P5/4Q1b1/4N3/8/ppp2ppp/2kr1bnr w KQkq - 0 1");
   expect(chess.in_check()).toBe(true);
@@ -26,11 +28,21 @@ test('double check', () => {
 });
 
 test('cross check', () => {
+	//Player 1 gets out of check, then the next player is now in check by discovered check
   var chess = new libraries.Chess();
   chess.load("R3Q2R/PP2KPPP/N1P5/8/2p3Q1/2p4n/p1q1kppp/r3r3 b KQkq - 0 1");
   expect(chess.in_check()).toBe(true);
   chess.move({from: 'e2', to: 'd2',});
   expect(chess.in_check()).toBe(true);
-//  console.log(chess.ascii());
+  console.log(chess.ascii());
 });
+
+test('moving into check', () => {
+  //Player tries to move into check, should not be allowed. No movement should occur
+	var chess = new libraries.Chess();
+  chess.load("3k4/1Q6/8/8/8/8/4K3/8 w KQkq - 0 1");
+  chess.move({from: 'd8', to: 'c8',});
+  expect(chess.fen()).toBe("3k4/1Q6/8/8/8/8/4K3/8 w KQkq - 0 1");
+});
+
 
